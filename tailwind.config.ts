3d6bc62b183d7f2fabfdc55229c7db9d6a32da35
subhash131 <subhashnayak131@gmail.com>
2024-11-05 22:50:53 +0530

    add shadcn button

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,22 +1,28 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "#000000",
-        foreground: "#1a1a1a",
-        dark: "#111111",
-        active: "#333333",
-        purple: "#634cc3",
-      },
-    },
+  	extend: {
+  		colors: {
+  			background: '#000000',
+  			foreground: '#1a1a1a',
+  			dark: '#111111',
+  			active: '#333333',
+  			purple: '#634cc3'
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
 export default config;
