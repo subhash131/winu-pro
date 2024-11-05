98f73fd5c3a7d3a67f15e1c14a2c5230840eefbf
subhash131 <subhashnayak131@gmail.com>
2024-11-05 17:24:50 +0530

    add new tournament component

--- src/components/tournaments/new-tournament.tsx
+++ src/components/tournaments/new-tournament.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const NewTournament = () => {
-  return <div className="w-full min-h-screen">NewTournament</div>;
+  return (
+    <div className="w-full h-full min-h-screen flex flex-col py-8 px-24 gap-4">
+      <h2 className="text-xl font-semibold">Host a Tournament</h2>
+      <div className="size-full flex">
+        <div className="size-96 bg-foreground shrink-0 rounded-xl border border-active"></div>
+        <div className="w-full"></div>
+      </div>
+    </div>
+  );
 };
 
 export default NewTournament;
