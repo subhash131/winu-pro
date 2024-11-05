ac88e8e72e63634222e0f80e1ac87c35b790e01a
subhash131 <subhashnayak131@gmail.com>
2024-11-06 02:23:20 +0530

    create user model

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,6 +1,32 @@
-import { Schema } from "mongoose";
+import mongoose, { Schema } from "mongoose";
 
 const userSchema = new Schema({
-  name: { type: String, required: true },
-  username: { type: String, required: true, unique: true },
+  name: { type: String, required: [true, "Please provide a name"] },
+  email: {
+    type: String,
+    required: [true, "Please provide an email"],
+    unique: true,
+  },
+  username: {
+    type: String,
+    required: [true, "Please provide a username"],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide a password"],
+  },
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
+  image: {
+    type: String,
+  },
+  forgotPasswordToken: String,
+  forgotPasswordTokenExpiry: String,
+  verifyToken: String,
+  verifyTokenExpiry: String,
 });
+
+export default mongoose.models.User || mongoose.model("User", userSchema);
