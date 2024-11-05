03002c565e197d8ed4f61f7c15e0e2d34d362fcb
subhash131 <subhashnayak131@gmail.com>
2024-11-06 02:31:16 +0530

    add db config

---
+++ src/db/config.ts
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+export async function connect() {
+  const MONGO_URI = process.env.MONGO_URI;
+  try {
+    if (!MONGO_URI) {
+      throw new Error("MONGO_URI not found!");
+    }
+    const { connection } = await mongoose.connect(MONGO_URI);
+
+    connection.on("connected", () => {
+      console.log("MongoDB Connected");
+    });
+    connection.on("error", (err) => {
+      console.log("Failed to connect with Mongo DB!" + err);
+      throw new Error(err);
+    });
+  } catch (e) {
+    console.log("Failed to connect with Mongo DB!" + e);
+  }
+}
