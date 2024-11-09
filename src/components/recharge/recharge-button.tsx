acad1371f17389de98ba9e3fb71cd26659a21462
subhash131 <subhashnayak131@gmail.com>
2024-11-09 19:40:48 +0530

    update recharge button

--- src/components/recharge/recharge-button.tsx
+++ src/components/recharge/recharge-button.tsx
@@ -1,9 +1,18 @@
 "use client";
 import React from "react";
 
-const RechargeButton = () => {
+const RechargeButton = ({
+  setShowRechargeModal,
+}: {
+  setShowRechargeModal: React.Dispatch<React.SetStateAction<boolean>>;
+}) => {
   return (
-    <button className="w-full py-2 bg-white text-black rounded-lg font-semibold">
+    <button
+      className="w-full py-2 bg-white text-black rounded-lg font-semibold"
+      onClick={() => {
+        setShowRechargeModal((prev) => !prev);
+      }}
+    >
       Recharge
     </button>
   );
