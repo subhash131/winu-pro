a3a0ea47f952d6694c6cc532f64bdea9fb492173
subhash131 <subhashnayak131@gmail.com>
2024-11-11 19:54:57 +0530

    update tournament create form

--- src/components/create-tournament/create-button.tsx
+++ src/components/create-tournament/create-button.tsx
@@ -1,17 +1,32 @@
+"use client";
 import React from "react";
 import { Tournament } from "../../types";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { createTournament } from "@/actions/tournament/create-tournament";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
 
-const CreateButton = ({
-  description,
-  endDate,
-  entryFee,
-  name,
-  startDate,
-  streamLink,
-  visibility,
-}: Tournament) => {
+const CreateButton = (tournament: Tournament) => {
+  const router = useRouter();
+  const queryClient = useQueryClient();
+  const mutation = useMutation({
+    mutationFn: createTournament,
+    onSuccess: (res) => {
+      toast.info(res.message);
+      queryClient.invalidateQueries({ queryKey: ["create-tournament"] });
+      router.replace(`/tournament/edit/${res.tournament._id}`);
+    },
+    onError: (error) => {
+      toast.error(error.message);
+      console.log(error);
+    },
+  });
   return (
-    <button className="bg-white px-6 py-2 rounded-lg text-black font font-semibold mt-10 active:scale-95 transition-transform">
+    <button
+      className={`bg-white px-6 py-2 rounded-lg text-black font font-semibold mt-10 active:scale-95 transition-transform disabled:scale-100 disabled:bg-gray-400`}
+      onClick={() => mutation.mutate(tournament)}
+      disabled={mutation.isPending}
+    >
       Create
     </button>
   );
--- src/components/create-tournament/tournament-details.tsx
+++ src/components/create-tournament/tournament-details.tsx
@@ -8,17 +8,38 @@ import CreateButton from "./create-button";
 import EntryFee from "./entry-fee";
 import { VisibilityT } from "@/types";
 import TournamentImage from "./tournament-image";
+import { useParams } from "next/navigation";
+import { useQuery } from "@tanstack/react-query";
 
 const TournamentDetails = () => {
+  const { id } = useParams();
+  const { isPending, error, data } = useQuery({
+    queryKey: ["repoData"],
+    queryFn: () =>
+      fetch("https://api.github.com/repos/TanStack/query").then((res) =>
+        res.json()
+      ),
+  });
+
   const [name, setName] = useState<string>("");
   const [streamLink, setStreamLink] = useState<string>("");
   const [description, setDescription] = useState<string>("");
   const [visibility, setVisibility] = useState<VisibilityT>("PUBLIC");
   const [startDate, setStartDate] = useState<Date | undefined>(new Date());
   const [endDate, setEndDate] = useState<Date | undefined>(new Date());
-  const [selectedEntryFee, setSelectedEntryFee] = useState(0);
+  const [selectedEntryFee, setSelectedEntryFee] = useState(10);
+
+  if (error) {
+    return <div>Failed to load! refresh the page again.</div>;
+  }
+
   return (
     <div className="flex gap-10">
+      {isPending && (
+        <div className="size-full absolute flex items-center justify-center top-0 left-0">
+          Loading...
+        </div>
+      )}
       <TournamentImage />
       <div className="size-full flex flex-col gap-2 pb-20">
         <Visibility setVisibility={setVisibility} visibility={visibility} />
