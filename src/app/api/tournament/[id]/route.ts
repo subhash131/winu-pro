a8427c0220969a419c1d5127d2e6bffcf8f098cf
subhash131 <subhashnayak131@gmail.com>
2024-11-12 00:48:08 +0530

    update route: updateTournamentById

--- src/app/api/tournament/[id]/route.ts
+++ src/app/api/tournament/[id]/route.ts
@@ -18,3 +18,49 @@ export async function GET(
     );
   }
 }
+
+export async function POST(
+  req: NextRequest,
+  { params: { id } }: { params: { id: string } }
+) {
+  const body = await req.json();
+
+  // Construct update object by including only the provided fields
+  const updateFields: Partial<{
+    name: string;
+    visibility: string;
+    streamLink: string;
+    entryFee: number;
+    startDate: Date;
+    endDate: Date;
+    description: string;
+    image: string;
+    matches: string[];
+    host: string;
+    isActive: boolean;
+  }> = {};
+
+  if (body.name) updateFields.name = body.name;
+  if (body.visibility) updateFields.visibility = body.visibility;
+  if (body.streamLink) updateFields.streamLink = body.streamLink;
+  if (body.entryFee) updateFields.entryFee = body.entryFee;
+  if (body.startDate) updateFields.startDate = body.startDate;
+  if (body.endDate) updateFields.endDate = body.endDate;
+  if (body.image) updateFields.image = body.image;
+  if (body.matches) updateFields.matches = body.matches;
+  if (body.host) updateFields.host = body.host;
+  if (typeof body.isActive === "boolean") updateFields.isActive = body.isActive;
+
+  updateFields.description = body.description;
+  // Update only provided fields
+  const tournament = await Tournament.findByIdAndUpdate(
+    id,
+    { $set: updateFields },
+    { new: true }
+  );
+
+  return NextResponse.json(
+    { message: "Tournament Updated", tournament },
+    { status: 200 }
+  );
+}
