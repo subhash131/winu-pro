f5166c23c1da43f78c84dfa9eb8b82ccb6f355a2
subhash131 <subhashnayak131@gmail.com>
2024-11-11 19:55:15 +0530

    update route: get user by  username

--- src/app/api/user/[username]/route.ts
+++ src/app/api/user/[username]/route.ts
@@ -1,6 +1,8 @@
 import { NextRequest, NextResponse } from "next/server";
 import User from "@/models/user";
+import { connect } from "@/db/config";
 
+connect();
 export async function GET(
   req: NextRequest,
   { params: { username } }: { params: { username: string } }
