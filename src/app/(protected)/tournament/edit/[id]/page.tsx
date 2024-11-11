06475e38954b8a28690603948b604fff677fa353
subhash131 <subhashnayak131@gmail.com>
2024-11-11 19:53:27 +0530

    create tournament edit

---
+++ src/app/(protected)/tournament/edit/[id]/page.tsx
@@ -0,0 +1,8 @@
+import TournamentForm from "@/components/create-tournament";
+import React from "react";
+
+const TournamentEditPage = () => {
+  return <TournamentForm />;
+};
+
+export default TournamentEditPage;
