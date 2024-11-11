9a3cd0af9cf013bb776c5afb44886906d63696c7
subhash131 <subhashnayak131@gmail.com>
2024-11-11 15:37:21 +0530

    add route create tournament

---
+++ src/app/api/tournament/create/route.ts
@@ -0,0 +1,70 @@
+import { NextRequest, NextResponse } from "next/server";
+import Tournament from "@/models/tournament";
+
+export async function POST(req: NextRequest) {
+  const body = await req.json();
+  const {
+    name,
+    visibility,
+    streamLink,
+    entryFee,
+    startDate,
+    endDate,
+    description,
+    image,
+    matches,
+    host,
+    isActive,
+  } = body;
+
+  if (!name)
+    return NextResponse.json({ message: "Name is required" }, { status: 400 });
+  if (!streamLink)
+    return NextResponse.json(
+      { message: "Stream link is required" },
+      { status: 400 }
+    );
+  if (!startDate)
+    return NextResponse.json(
+      { message: "Start date is required" },
+      { status: 400 }
+    );
+  if (!endDate)
+    return NextResponse.json(
+      { message: "End date is required" },
+      { status: 400 }
+    );
+  if (!host)
+    return NextResponse.json({ message: "Host is required" }, { status: 400 });
+  if (!matches)
+    return NextResponse.json(
+      { message: "Matches are required" },
+      { status: 400 }
+    );
+  if (!image)
+    return NextResponse.json({ message: "Image is required" }, { status: 400 });
+  if (!entryFee)
+    return NextResponse.json(
+      { message: "Entry Fee is required" },
+      { status: 400 }
+    );
+
+  const tournament = new Tournament({
+    name,
+    visibility,
+    streamLink,
+    entryFee,
+    startDate,
+    endDate,
+    description,
+    image,
+    matches,
+    host,
+    isActive,
+  });
+  tournament.save();
+  return NextResponse.json(
+    { message: "New Tournament Created", tournament },
+    { status: 201 }
+  );
+}
