8b72c2f712bce5c563995ad7960209b6c242b944
subhash131 <subhashnayak131@gmail.com>
2024-11-11 16:37:31 +0530

    update component: CreateButton

--- src/components/create-tournament/create-button.tsx
+++ src/components/create-tournament/create-button.tsx
@@ -1,6 +1,15 @@
 import React from "react";
+import { Tournament } from "../../types";
 
-const CreateButton = () => {
+const CreateButton = ({
+  description,
+  endDate,
+  entryFee,
+  name,
+  startDate,
+  streamLink,
+  visibility,
+}: Tournament) => {
   return (
     <button className="bg-white px-6 py-2 rounded-lg text-black font font-semibold mt-10 active:scale-95 transition-transform">
       Create
