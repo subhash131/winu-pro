08663aeb06c9c66db622a20d579c4a94c6d4fa64
subhash131 <subhashnayak131@gmail.com>
2024-11-11 21:43:56 +0530

    update action: create tournament

--- src/actions/tournament/create-tournament.ts
+++ src/actions/tournament/create-tournament.ts
@@ -2,9 +2,13 @@
 import { Tournament } from "@/types";
 
 export async function createTournament(tournament: Tournament) {
-  const res = await fetch("http://localhost:3000/api/tournament/create", {
-    method: "POST",
-    body: JSON.stringify(tournament),
-  }).then(async (res) => await res.json());
-  return res;
+  try {
+    const res = await fetch("http://localhost:3000/api/tournament/create", {
+      method: "POST",
+      body: JSON.stringify(tournament),
+    }).then(async (res) => await res.json());
+    return res;
+  } catch (e) {
+    return { message: e };
+  }
 }
