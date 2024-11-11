482ce2c14a231480f0b3668b5d3c1235da5834e2
subhash131 <subhashnayak131@gmail.com>
2024-11-11 22:28:35 +0530

    fix schedule

--- src/components/create-tournament/schedule.tsx
+++ src/components/create-tournament/schedule.tsx
@@ -1,29 +1,39 @@
 "use client";
 import { DatePicker } from "@/components/ui/date-picker";
-import React, { useState } from "react";
-
-type ScheduleProps = {
-  startDate: Date | undefined;
-  setStartDate: React.Dispatch<React.SetStateAction<Date | undefined>>;
-  endDate: Date | undefined;
-  setEndDate: React.Dispatch<React.SetStateAction<Date | undefined>>;
-};
-
-const Schedule = ({
-  setStartDate,
-  startDate,
-  endDate,
+import {
   setEndDate,
-}: ScheduleProps) => {
+  setStartDate,
+} from "@/state-manager/features/tournament-form";
+import { RootState } from "@/state-manager/store";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+const Schedule = () => {
+  const { startDate, endDate } = useSelector(
+    (state: RootState) => state.tournamentForm
+  );
+  const dispatch = useDispatch();
+  const [localStartDate, setLocalStartDate] = useState<Date | undefined>(
+    startDate ? new Date(startDate) : undefined
+  );
+  const [localEndDate, setLocalEndDate] = useState(
+    endDate ? new Date(endDate) : undefined
+  );
+
+  useEffect(() => {
+    if (localStartDate) dispatch(setStartDate(localStartDate.toISOString()));
+    if (localEndDate) dispatch(setEndDate(localEndDate.toISOString()));
+  }, [localStartDate, localEndDate]);
+
   return (
     <div className="h-fit gap-2 w-fit rounded-lg bg-active flex flex-col overflow-hidden py-2 px-3">
       <div className="w-full h-full flex gap-4 items-center justify-between">
         <p>Start</p>
-        <DatePicker setDate={setStartDate} date={startDate} />
+        <DatePicker setDate={setLocalStartDate} date={localStartDate} />
       </div>
       <div className="w-full h-full flex gap-4 items-center justify-between">
         <p>End</p>
-        <DatePicker setDate={setEndDate} date={endDate} />
+        <DatePicker setDate={setLocalEndDate} date={localEndDate} />
       </div>
     </div>
   );
