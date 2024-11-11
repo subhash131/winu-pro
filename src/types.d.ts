78092c5006b5bc038c69ddb7191638e2cdd7537d
subhash131 <subhashnayak131@gmail.com>
2024-11-11 16:36:50 +0530

    create global types

---
+++ src/types.d.ts
@@ -0,0 +1,15 @@
+export type VisibilityT = "PUBLIC" | "PRIVATE";
+
+export type Tournament = {
+  name: string;
+  streamLink: string;
+  description: string;
+  visibility: Visibility;
+  image: string;
+  startDate?: Date;
+  endDate?: Date;
+  entryFee: number;
+  isActive: boolean;
+  host: string;
+  matches: string[];
+};
