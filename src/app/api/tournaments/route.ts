103c9ed32102e23abe5f47603ebaadc2f5251fdd
subhash131 <subhashnayak131@gmail.com>
2024-11-12 13:40:31 +0530

    create route get all tournament

--- src/app/api/tournaments/route.ts
+++ src/app/api/tournaments/route.ts
@@ -1,11 +1,16 @@
 import { NextResponse } from "next/server";
 import Tournament from "@/models/tournament";
+import User from "@/models/user";
 import { connect } from "@/db/config";
 
 connect();
 export async function GET() {
   try {
-    const tournaments = await Tournament.find();
+    User; //load the model
+    const tournaments = await Tournament.find().populate(
+      "host",
+      "name email username isVerified image"
+    );
     return NextResponse.json({ tournaments }, { status: 200 });
   } catch (e) {
     return NextResponse.json(
